ca2833fbbf5c10b16eb6f274c78703d41fa28b3c
Christoph Fricke <christoph@frickeonline.de>
2023-10-18 18:19:50 +0200

    feat: extract package exports into exports folder

---
+++ exports/main.ts
@@ -0,0 +1,4 @@
+export { createOpenApiHttp } from "../src/openapi-http.js";
+export type { HttpOptions, OpenApiHttpHandlers } from "../src/openapi-http.js";
+
+export type { AnyApiSpec } from "../src/type-helpers.js";
