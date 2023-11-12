fd545ce246e4cadebb8e3ff0208a41f9b0895141
Christoph Fricke <christoph@frickeonline.de>
2023-11-12 18:46:10 +0100

    test: split test suites and run type-checks for integration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,25 @@
 import { name } from "./package.json";
 import { defineProject } from "vitest/config";
 
-export default defineProject({ test: { name } });
+const suite = process.env["TEST_SUITE"];
+
+function getTestDir(suite?: string): string | undefined {
+  switch (suite) {
+    case "unit":
+      return "src";
+    case "integration":
+      return "test";
+    default:
+      return undefined;
+  }
+}
+
+export default defineProject({
+  test: {
+    name: suite ?? name,
+    dir: getTestDir(suite),
+    typecheck: {
+      include: ["**/*.test.ts"],
+    },
+  },
+});
