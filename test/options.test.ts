98422e5bb6e57e308fe513f473ef96f1ff0930d4
Christoph Fricke <christoph@frickeonline.de>
2023-11-12 18:22:15 +0100

    test(integration): add factory options tests

---
+++ test/options.test.ts
@@ -0,0 +1,24 @@
+import { HttpResponse } from "msw";
+import { describe, expect, test } from "vitest";
+import { createOpenApiHttp } from "../exports/main.js";
+import type { paths } from "./fixtures/options.api.js";
+
+describe("Given a created HTTP object with options", () => {
+  const http = createOpenApiHttp<paths>({ baseUrl: "http://localhost:3000" });
+
+  test("When a OpenAPI handler is created, Then the baseUrl is prepended to the path", async () => {
+    const handler = http.get("/resource", () => {
+      return HttpResponse.json({ id: "test-id" });
+    });
+
+    expect(handler.info.path).toBe("http://localhost:3000/resource");
+  });
+
+  test("When a vanilla handler is created, Then the baseUrl is not prepended to the path", async () => {
+    const handler = http.untyped.get("/some-resource", () => {
+      return HttpResponse.json();
+    });
+
+    expect(handler.info.path).toBe("/some-resource");
+  });
+});
